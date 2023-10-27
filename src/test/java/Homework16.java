475dbf5adfbf3289a832c334a6ce70ee2641bf0a
Daria2906 <daria.chebotnyagina@testpro.io>
2023-10-27 11:43:41 -0500

    Method registrationNavigation

--- src/test/java/Homework16.java
+++ src/test/java/Homework16.java
@@ -12,11 +12,12 @@ public class Homework16 extends BaseTest{
     @Test
     public void registrationNavigation(){
 
-            ChromeOptions options = new ChromeOptions();
-            options.addArguments("--remote-allow-origins=*");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
 
-            WebDriver driver = new ChromeDriver(options);
-            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        WebDriver driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
 
             String url = "https://qa.koel.app/";
             driver.get(url);
