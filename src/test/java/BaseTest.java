34cde535043e53c6cfb729fe079d7621b1adfc2e
OneAndOnlyAdam <130710009+OneAndOnlyAdam@users.noreply.github.com>
2023-10-28 13:21:38 -0600

    Homework18
    First time running via Terminal in IntelliJ:  Shows build successful so I guess that's good.

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,11 +1,29 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.BeforeSuite;
 
 public class BaseTest {
 
+    public WebDriver driver = null;
     @BeforeSuite
     static void setupClass() {
         WebDriverManager.chromedriver().setup();
+    }
+    public void provideEmail(String email){
+        WebElement emailField = driver.findElement(By.cssSelector("input[type='email']"));
+        emailField.clear();
+        emailField.sendKeys(email);
+    }
+    public void providePassword(String password){
+        WebElement passwordField = driver.findElement(By.cssSelector("input[type='passowrd']"));
+        passwordField.clear();
+        passwordField.sendKeys(password);
+    }
+    public void clickSubmit(){
+        WebElement submit = driver.findElement(By.cssSelector("button[type='submit'"));
+        submit.click();
     }
 }
