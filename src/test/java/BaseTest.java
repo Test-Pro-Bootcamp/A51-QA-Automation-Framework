4fcf93fd57e9a9862a709f3d7c6b7145ba154841
arleishas <148395284+arleishas@users.noreply.github.com>
2023-10-30 16:41:27 -0700

    A51-L19-Class-Demo

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -4,9 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.*;
 
 import java.time.Duration;
 
@@ -21,12 +19,14 @@ public class BaseTest {
         WebDriverManager.chromedriver().setup();
     }
     @BeforeMethod
-    public void launchBrowser(){
+    @Parameters({"BaseURL"})
+    public void launchBrowser(String BaseURL){
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*");
         driver = new ChromeDriver(options);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.manage().window().maximize();
+        navigateToLoginPage(BaseURL);
     }
     @AfterMethod
     public void closeBrowser(){
@@ -35,6 +35,9 @@ public class BaseTest {
 
     public void navigateToLoginPage(){
         driver.get(url);
+    }
+    public void navigateToLoginPage(String BaseURL){
+        driver.get(BaseURL);
     }
 
     public void provideEmail(String email){
--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -1,20 +1,31 @@
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
-
-import java.time.Duration;
 
 public class LoginTests extends BaseTest {
 
+    @DataProvider(name = "LoginData")
+    public Object [][]getDataFromDataProvider(){
+        return new Object[][] {
+                {"demo@class.com", "te$t$tudent"},
+                {"invalidemail@class.com", "te$t$tudent"},
+                {"demo@class.com", "InvalidPassword"},
+                {"", ""}
+        };
 
-    @Test
-    public void loginEmptyEmailPassword() {
+    }
+
+
+    @Test(dataProvider = "LoginData")
+    public void loginTests(String email, String password) throws InterruptedException{
        navigateToLoginPage();
+       provideEmail(email);
+       providePassword(password);
        clickSubmit();
+
+       Thread.sleep(2000);
        Assert.assertEquals(driver.getCurrentUrl(), url);
     }
      @Test
@@ -41,9 +52,10 @@ public class LoginTests extends BaseTest {
         Assert.assertTrue(avatar.isDisplayed());
     }
     @Test
-    public void loginValidEmailEmptyPassword() {
+    public void loginValidEmailInvalidPassword() {
         navigateToLoginPage();
         provideEmail("demo@class.com");
+        providePassword("InvalidPassword");
         clickSubmit();
         //Expected Result
         Assert.assertEquals(driver.getCurrentUrl(), url);
