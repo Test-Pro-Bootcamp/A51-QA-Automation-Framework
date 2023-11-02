28de021fe2f867a95ba468d7088dcd676ac098a2
KhrysCS <148722222+KhrysCS@users.noreply.github.com>
2023-11-02 08:24:29 -0500

    Homework #17: TestNG Overview Completed
    Refactorized Homewor17.java and added comments.

--- src/test/java/Homework17.java
+++ src/test/java/Homework17.java
@@ -7,30 +7,14 @@ import org.testng.annotations.Test;
 
 public class Homework17 extends BaseTest {
 
-    /*
-    TODO Homework #17: TestNG Overview
-     *1. Create a new branch named homework-17.
-     *2. Create a new file, Homework17.java in IntelliJ IDEA.
-      3. Create a test case, addSongToPlaylist() using @Test annotation and the helper/reusable methods we created.
-         *4. Navigate to "https://qa.koel.app/".
-        * 5. Log in with your credentials.
-         6. Search for a song (choose any song of your choice).
-         7. Click 'View All' button to display the search results.
-         8. Click the first song in the search results.
-         9. Click 'ADD TO...' button.
-         10. Choose the playlist to add it to, (you can create a new one with a unique name).
-         11. Verify that a notification message appears and contains the text, "Added 1 song into {your playlist}".
-         Note: For verification, please use the Assert.assertEquals() method to compare the actual notification message text with the expected text. If they match, the assertion will pass; otherwise, it will fail.
-         12. Commit your changes to the new branch homework-17.
-         13. Push your code to the remote repository.
-         14. Create a pull request.
-         Note: Your work will be auto graded so you can continue with the next assignment in less than a minute (just refresh the results page).
- */
+    /**
+     * Test case to add a song to a playlist
+     * @throws InterruptedException
+     */
     @Test
     public void addSongToPlaylist() throws InterruptedException{
 
-
-        String uniquePlaylist = "HW17PL_"+ RandomString.make().substring(0,6);
+        String uniquePlaylist = generateNewPlaylistName();
 
         navigateToPage();
         provideEmail("demo@class.com");
@@ -38,61 +22,149 @@ public class Homework17 extends BaseTest {
         clickSubmit();
 
         //Creating Unique Playlist
-        WebElement addPLBtn = driver.findElement(By.cssSelector("i[data-testid='sidebar-create-playlist-btn']"));
-        addPLBtn.click();
-        Thread.sleep(3000);
+        clickSideBarCreatePlaylistBtn();
+
+        Thread.sleep(2000);
 
         //Select New Playlist Option
-        WebElement newPLBtn = driver.findElement(By.cssSelector("li[data-testid='playlist-context-menu-create-simple']"));
-        newPLBtn.click();
+        clickMenuOptionNewPlaylist();
+
         Thread.sleep(2000);
 
         //Enter new playlist name
-         WebElement playListField = driver.findElement(By.cssSelector("input[name='name']"));
-         playListField.clear();
-         playListField.sendKeys(uniquePlaylist);//HW17PL
-         Thread.sleep(2000);
-         playListField.sendKeys(Keys.ENTER);
-         Thread.sleep(2000);
+        provideNewPlaylistName(uniquePlaylist);
 
-
-
-        //TODO: Search for a song (choose any song of your choice).
-        WebElement searchField = driver.findElement(By.cssSelector("input[type='search']"));
-        searchField.clear();
-        searchField.sendKeys("Dark Days");//Dark Days
         Thread.sleep(2000);
 
-        //TODO: Click 'View All' button to display the search results.
-        WebElement viewAllBtn = driver.findElement(By.cssSelector("button[data-test='view-all-songs-btn']"));
-        viewAllBtn.click();
+        provideSongNameForSearch();
+
         Thread.sleep(2000);
 
+        clickSearchResultsViewAllBtn();
 
-        //TODO: Click the first song in the search results.
-        WebElement firstSong = driver.findElement(By.cssSelector("#songResultsWrapper table.items tr:nth-child(1)"));
-        firstSong.click();
         Thread.sleep(2000);
 
-        //TODO: Click 'ADD TO...' button.
-        WebElement addToBtn = driver.findElement(By.cssSelector("button.btn-add-to"));
-        addToBtn.click();
+        clickSearchResultsFirstSong();
+
+        Thread.sleep(2000);
+
+        clickSelectedSongAddToBtn();
+
+        Thread.sleep(2000);
+
+        clickOnPlaylistNameFromAddToMenu(uniquePlaylist);
+
         Thread.sleep(3000);
 
 
-        //TODO: Choose the playlist to add it to, (you can create a new one with a unique name).
-        WebElement selectPL = driver.findElement(By.xpath("//section[@id='songResultsWrapper'] //li[contains( . , '"+uniquePlaylist+"')]"));
-        selectPL.click();
-        Thread.sleep(3000);
+        //Expected Results
+        String updatedUniquePlaylist = updateUniquePlaylistString(uniquePlaylist);
+        String msgDisplayed = createValidNotificationMessageString(updatedUniquePlaylist);
 
-
-        //TODO: Verify that a notification message appears and contains the text, "Added 1 song into {your playlist}".
         WebElement notificationMsg = driver.findElement(By.cssSelector("div.alertify-logs div.success"));//div.alertify-logs div.success
-        uniquePlaylist = '"'+uniquePlaylist+"."+'"';
-        String msgDisplayed = "Added 1 song into "+uniquePlaylist;
 
         Assert.assertEquals(msgDisplayed,notificationMsg.getText());
 
     }
+
+    /**
+     * Adds quotations marks and period to the uniquePlaylist String provided
+     * @param uniquePlaylist the original playlist name that should be modified.
+     * @return a String with provided playlist name concatenated to quotation marks and a period.
+     */
+    private String updateUniquePlaylistString(String uniquePlaylist) {
+        return '"'+uniquePlaylist+"."+'"';
+    }
+
+    /**
+     * Finds the element list item that contains the new playlist added and clicks on the item.
+     * @param uniquePlaylist Name of the new playlist created
+     */
+    private void clickOnPlaylistNameFromAddToMenu(String uniquePlaylist) {
+        WebElement selectPL = driver.findElement(By.xpath("//section[@id='songResultsWrapper'] //li[contains( . , '"+uniquePlaylist+"')]"));
+        selectPL.click();
+    }
+
+    /**
+     *  Finds the element of the Add To button that appears when a song from the search results list is clicked on
+     *  and clicks on the button.
+    **/
+    private void clickSelectedSongAddToBtn() {
+        WebElement addToBtn = driver.findElement(By.cssSelector("button.btn-add-to"));
+        addToBtn.click();
+    }
+
+    /**
+     * Finds the first song from the search results list and clicks on it.
+     */
+    private void clickSearchResultsFirstSong() {
+        WebElement firstSong = driver.findElement(By.cssSelector("#songResultsWrapper table.items tr:nth-child(1)"));
+        firstSong.click();
+    }
+
+    /**
+     * Finds the View All button from the search results list view and clicks on it.
+     */
+    private void clickSearchResultsViewAllBtn() {
+        WebElement viewAllBtn = driver.findElement(By.cssSelector("button[data-test='view-all-songs-btn']"));
+        viewAllBtn.click();
+    }
+
+    /**
+     * Finds the search textbox field and enters the song to search for. Once song is entered
+     * the search begins and results are displayed.
+     */
+    private void provideSongNameForSearch() {
+        WebElement searchField = driver.findElement(By.cssSelector("input[type='search']"));
+        searchField.clear();
+        searchField.sendKeys("Dark Days");//Dark Days
+    }
+
+    /**
+     * Find the input textbox to enter the new playlist name on the left side menu. The textbox appears when the "+" is clicked
+     * and the options "New Playlist" is selected.
+     * @param uniquePlaylist is the name of the new playlist that will be created and entered into the textbox.
+     */
+    private void provideNewPlaylistName(String uniquePlaylist) {
+        WebElement playListField = driver.findElement(By.cssSelector("input[name='name']"));
+        playListField.clear();
+        playListField.sendKeys(uniquePlaylist);
+        playListField.sendKeys(Keys.ENTER);
+    }
+
+    /**
+     * Finds the element for the new playlist option from the menu that appears in the leftside menu
+     * when the "+" is clicked and clicks on the element.
+     */
+    private void clickMenuOptionNewPlaylist() {
+        WebElement newPLBtn = driver.findElement(By.cssSelector("li[data-testid='playlist-context-menu-create-simple']"));
+        newPLBtn.click();
+    }
+
+    /**
+     * Finds the element for the "+" button from the left side menu and clicks on it to add a new playlist.
+     */
+    private void clickSideBarCreatePlaylistBtn() {
+        WebElement addPLBtn = driver.findElement(By.cssSelector("i[data-testid='sidebar-create-playlist-btn']"));
+        addPLBtn.click();
+    }
+
+    /**
+     * Creates a random string and concatenates it to a base playlist name to generate a unique playlist name.
+     * @return the new uniquie playlist name String
+     */
+    private String generateNewPlaylistName() {
+        return "HW17PL_"+ RandomString.make().substring(0,6);
+    }
+
+    /**
+     * Creates the expected result valid notification message string.
+     * @param updatedUniquePlaylist the unique playlist name updated with the quotation marks and period.
+     * @return the full valid notification message string created.
+     */
+    private String createValidNotificationMessageString(String updatedUniquePlaylist){
+        return "Added 1 song into "+updatedUniquePlaylist;
+    }
+
 }
 
