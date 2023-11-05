461513826ab5698169d5fb7624514f3a9f228559
aparajita135 <142899090+aparajita135@users.noreply.github.com>
2023-11-04 23:24:57 -0400

    revised Homework

--- src/test/java/Homework21.java
+++ src/test/java/Homework21.java
@@ -14,7 +14,7 @@ public class Homework21 extends BaseTest{
 
     @Test
 public void renamePlaylist(){
-    String getNewPlaylistname = "Updated playlist 'test.'";
+    String getNewPlaylistName = "Updated playlist \"test.\"";
 
     provideEmail("aparajita.jha@testpro.io");
     providePassword("testpro135@");
@@ -23,17 +23,17 @@ public void renamePlaylist(){
     contextClickPlaylist();
     clickEdit();
     newPlaylistName();
-    //Assert.assertEquals(getRenamePlaylistSuccessMsg() , getNewPlaylistName);
+    Assert.assertEquals(getRenamePlaylistSuccessMsg() , getNewPlaylistName);
 }
 
     public void contextClickPlaylist() {
 
-        WebElement playlist = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".playlist:nth-child(3)")));
+        WebElement playlist = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id='playlists']/ul/li[4]")));
         actions.contextClick(playlist).perform();
 
     }
     public void clickEdit(){
-        WebElement edit=wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id='playlists']/ul/li[3]/nav/ul/li[1]")));
+        WebElement edit=wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id='playlists']/ul/li[4]/nav/ul/li[1]")));
      actions.click(edit).perform();
     }
     public void newPlaylistName(){
