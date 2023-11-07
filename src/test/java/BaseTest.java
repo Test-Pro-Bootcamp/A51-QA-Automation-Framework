94baed890c02eb7a657cfe5dbfe742177e5a9949
Daria2906 <daria.chebotnyagina@testpro.io>
2023-11-07 12:10:24 -0600

    change 1

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,14 +1,23 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeSuite;
+import org.testng.ITest;
+import org.testng.Reporter;
+import org.testng.annotations.*;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.time.Duration;
+
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFCell;
 
 public class BaseTest {
 
@@ -16,18 +25,39 @@ public class BaseTest {
 
    public String url = "https://qa.koel.app/";
 
+    @DataProvider(name="LoginData")
+    public Object[][] getDataFromDataProviders() {
+        return new Object[][]{
+                {"daria.chebotnyagina@testpro.io", "Asdfasdf1"},
+                {"invalidEmail@testpro.io", "Asdfasdf1"},
+                {"daria.chebotnyagina@testpro.io", "InvalidPassword"},
+                {"",""}
+        };
+    }
+
+    /*
+    @DataProvider(name="excel-data")
+    public Object[][] excelLDP() throws IOException{
+        Object[][] arrObj = getExcelData("\\src\\test\\resources\\test.xlsx", "test.xlsx");
+        return arrObj;
+    }*/
+
     @BeforeSuite
     static void setupClass() {
         WebDriverManager.chromedriver().setup();
     }
  @BeforeMethod
-    public void launchBrowser(){
+ @Parameters({"BaseURL"})
+    public void launchBrowser(String BaseURL){
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*");
 
         driver = new ChromeDriver(options);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.manage().window().maximize();
+        //url = BaseURL;
+        navigateToLoginPage(BaseURL);
+
     }
 @AfterMethod
     public void closeBrowser(){
@@ -36,6 +66,9 @@ public class BaseTest {
 
     public void navigateToLoginPage(){
         driver.get(url);
+    }
+    public void navigateToLoginPage(String BaseURL){
+        driver.get(BaseURL);
     }
 
     public void provideEmail(String email){
@@ -53,6 +86,42 @@ public class BaseTest {
     public void clickSubmit(){
         WebElement submit = driver.findElement(By.cssSelector("button[type='submit']"));
         submit.click();
-
     }
+
+    /*
+    @Test(dataProvider = "excel-data")
+    public void search(String keyword1, String keyword2){
+        WebElement txtBox = driver.findElement(By.tagName("//input[@class='gLFYf gsfi']"));
+        txtBox.sendKeys(keyword1, keyword2);
+        Reporter.log("keyWord Entered is:"+keyword1+ " " +keyword2);
+        txtBox.sendKeys(Keys.ENTER);
+        Reporter.log("Search results are displayed.");
+    }
+
+    public String[][] getExcelData(String fileName, String sheetName){
+        String [][] data = null;
+        try{
+            FileInputStream fileInputStream = new FileInputStream(fileName);
+            XSSFWorkbook wb = new XSSFWorkbook(fileInputStream);
+            XSSFSheet sheet = wb.getSheet(sheetName);
+            XSSFRow row = sheet.getRow(0);
+
+            int numOfRows = sheet.getPhysicalNumberOfRows();
+            int numOfColumns = row.getLastCellNum();
+
+            XSSFCell cell;
+            data = new String[numOfRows-1][numOfColumns];
+
+            for(int i=1; i<numOfRows; i++) {
+            for(int j=0; j<numOfColumns; j++){
+                row = sheet.getRow(i);
+                cell = row.getCell(j);
+                data [i-1][j] = cell.getStringCellValue();
+              }
+            }
+        }  catch (Exception e) {
+            System.out.println("Something went wrong." +e);
+        }
+        return data;
+    }*/
 }
--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -1,15 +1,22 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class LoginTests extends BaseTest {
 
-    @Test
-    public void loginEmptyEmailPassword() {
-navigateToLoginPage();
-clickSubmit();
-        Assert.assertEquals(driver.getCurrentUrl(), url);
+
+    @Test(dataProvider = "LoginData")
+    public void loginTests(String email, String password) throws InterruptedException{
+    navigateToLoginPage();
+    provideEmail(email);
+    providePassword(password);
+    clickSubmit();
+    Thread.sleep(2000);
+    //Assert.assertEquals(driver.getCurrentUrl(), url);
+    WebElement avatar = driver.findElement(By.cssSelector("img[class='avatar']"));
+    Assert.assertTrue(avatar.isDisplayed());
 
     }
 
@@ -23,4 +30,26 @@ clickSubmit();
         WebElement avatar = driver.findElement(By.cssSelector("img[class='avatar']"));
         Assert.assertTrue(avatar.isDisplayed());
     }
+    @Test
+    public void loginInvalidEmailValidPassword(){
+        navigateToLoginPage();
+        provideEmail("invalidEmail@testpro.io");
+        providePassword("Asdfasdf1");
+        clickSubmit();
+
+        WebElement avatar = driver.findElement(By.cssSelector("img[class='avatar']"));
+        Assert.assertTrue(avatar.isDisplayed());
+    }
+
+    @Test
+    public void loginValidEmailInvalidPassword() throws InterruptedException{
+        navigateToLoginPage();
+        provideEmail("daria.chebotnyagina@testpro.io");
+        providePassword("InvalidPassword");
+        clickSubmit();
+
+        WebElement avatar = driver.findElement(By.cssSelector("img[class='avatar']"));
+        Assert.assertTrue(avatar.isDisplayed());
+    }
+
 }
