3b0ffaab5d6847f229fae05ea12bc026a6da6d40
Daria2906 <daria.chebotnyagina@testpro.io>
2023-11-07 12:28:39 -0600

    change 2

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Parameters;
 
 import java.time.Duration;
 
@@ -21,7 +22,8 @@ public class BaseTest {
         WebDriverManager.chromedriver().setup();
     }
  @BeforeMethod
-    public void launchBrowser(){
+ @Parameters({"BaseURL"})
+    public void launchBrowser(String BaseUrl){
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*");
 
