9177623491de44c73c7a6ae74ed613e47b730082
Sdet24 <149759509+Sdet24@users.noreply.github.com>
2023-11-07 21:03:40 -0600

    Update LoginTests.java

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -1,4 +1,6 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
@@ -21,5 +23,27 @@ public class LoginTests extends BaseTest {
         driver.get(url);
         Assert.assertEquals(driver.getCurrentUrl(), url);
         driver.quit();
+    }
+
+    @Test
+    public void registrationNavigation(){
+    // Pre-Conditions
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2000));
+
+    // Steps
+        String url = "https://qa.koel.app/";
+        driver.get(url);
+
+        WebElement RegistrationLink = driver.findElement(By.cssSelector("[href='registration]"));
+        String registrationUrl = "https://qa.koel.app/registration";
+
+    // Expected Results
+        Assert.assertEquals(driver.getCurrentUrl(), registrationUrl);
+        driver.quit();
+
     }
 }
