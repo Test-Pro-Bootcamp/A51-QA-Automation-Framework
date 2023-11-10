623348cc2b1847e0b0f1bcdccebeefcd5a3c81ad
Daria2906 <daria.chebotnyagina@testpro.io>
2023-11-10 00:19:33 -0600

    change 1

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -20,14 +21,20 @@ import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFCell;
 
+import javax.swing.*;
 
 
 public class BaseTest {
 
+    //References start
    public WebDriver driver;
    public WebDriverWait wait;
    public String url;
 
+   Actions action;
+   //References end
+
+    //DataProvider start
     @DataProvider(name="LoginData")
     public Object[][] getDataFromDataProviders() {
         return new Object[][]{
@@ -36,54 +43,6 @@ public class BaseTest {
                 {"daria.chebotnyagina@testpro.io", "InvalidPassword"},
                 {"",""}
         };
-    }
-
-    @BeforeSuite
-    static void setupClass() {
-        WebDriverManager.chromedriver().setup();
-    }
- @BeforeMethod
- @Parameters({"BaseURL"})
-    public void launchBrowser(String BaseURL){
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--remote-allow-origins=*");
-
-        driver = new ChromeDriver(options);
-        //driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10)); - Implicitly Wait
-     //Explicitly Wait:
-        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        driver.manage().window().maximize();
-        url = BaseURL;
-        navigateToLoginPage();
-
-    }
-@AfterMethod
-    public void closeBrowser(){
-        driver.quit();
-    }
-
-    public void navigateToLoginPage(){
-        driver.get(url);
-    }
-
-    public void provideEmail(String email){
-        WebElement emailField = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("input[type='email']")));
-        //WebElement emailField = driver.findElement(By.cssSelector("input[type='email']"));
-        emailField.clear();
-        emailField.sendKeys(email);
-    }
-
-    public void providePassword(String password){
-        WebElement passwordField = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("input[type='password']")));
-        //WebElement passwordField = driver.findElement(By.cssSelector("input[type='password']"));
-        passwordField.clear();
-        passwordField.sendKeys(password);
-    }
-
-    public void clickSubmit(){
-        WebElement submit = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("button[type='submit']")));
-        //WebElement submit = driver.findElement(By.cssSelector("button[type='submit']"));
-        submit.click();
     }
 
 
@@ -120,4 +79,55 @@ public class BaseTest {
         }
         return data;
     }
+    //DataProvider end
+
+    @BeforeSuite
+    static void setupClass() {
+        WebDriverManager.chromedriver().setup();
+    }
+ @BeforeMethod
+ @Parameters({"BaseURL"})
+    public void launchBrowser(String BaseURL){
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+
+        driver = new ChromeDriver(options);
+        //driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10)); - Implicitly Wait
+     //Explicitly Wait:
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        action = new Actions(driver);
+        driver.manage().window().maximize();
+        url = BaseURL;
+        navigateToLoginPage();
+
+    }
+@AfterMethod
+    public void closeBrowser(){
+        driver.quit();
+    }
+
+    public void navigateToLoginPage(){
+        driver.get(url);
+    }
+
+    public void provideEmail(String email){
+        WebElement emailField = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("input[type='email']")));
+        //WebElement emailField = driver.findElement(By.cssSelector("input[type='email']"));
+        emailField.clear();
+        emailField.sendKeys(email);
+    }
+
+    public void providePassword(String password){
+        WebElement passwordField = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("input[type='password']")));
+        //WebElement passwordField = driver.findElement(By.cssSelector("input[type='password']"));
+        passwordField.clear();
+        passwordField.sendKeys(password);
+    }
+
+    public void clickSubmit(){
+        WebElement submit = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("button[type='submit']")));
+        //WebElement submit = driver.findElement(By.cssSelector("button[type='submit']"));
+        submit.click();
+    }
+
 }
