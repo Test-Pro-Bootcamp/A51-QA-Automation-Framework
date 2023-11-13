7b4e7d2872d822251332d42c7dd59928d2accf1a
Sanjeela <sanjeela.chitrakar@testpro.io>
2023-11-13 09:51:47 +0545

    Changes In Excel DP
    Added assertion

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -1,23 +1,25 @@
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
+import org.testng.Reporter;
 import org.testng.annotations.Test;
-
-import java.time.Duration;
 
 public class LoginTests extends BaseTest {
 
 
-    @Test
-    public void loginEmptyEmailPassword() {
 
 
+    @Test(dataProvider = "LoginData")
+    public void loginTests(String email, String password) throws InterruptedException {
         navigateToLoginPage();
+        provideEmail(email);
+        providePassword(password);
         clickSubmit();
+        Thread.sleep(2000);
         Assert.assertEquals(driver.getCurrentUrl(), url);
+
+
 
 
     }
@@ -56,8 +58,30 @@ public class LoginTests extends BaseTest {
         WebElement avatar = driver.findElement(By.cssSelector("img[class='avatar']"));
         //Expected Result
         Assert.assertTrue(avatar.isDisplayed());
+    }
 
-
-
+    @Test(dataProvider = "excel-data")
+    public void loginWithExcelData(String email, String password) throws InterruptedException {
+        try {
+            //Step 1:  Enter Email
+            Thread.sleep(2000);
+            navigateToLoginPage();
+            provideEmail(email);
+            //Step 2:  Enter password
+            providePassword(password);
+            Thread.sleep(2000);
+            // WebElement txtBox = driver.findElement(By.tagName("//input[@class='gLFYf gsfi']"));
+            // txtBox.sendKeys(email,password);
+           // Reporter.log("KeyWord Entered is:" + email + " " + password);
+            // txtBox.sendKeys(Keys.ENTER);
+            //Step 3:Click on Submit Button
+            clickSubmit();
+            WebElement avatar = driver.findElement(By.cssSelector("img[class='avatar']"));
+            //Expected Result
+            Assert.assertTrue(avatar.isDisplayed());
+            // Reporter.log("Successfully Logged in.");
+        }catch (Exception e){
+            Reporter.log("Unable to login withexcel data for an unknown reason." +e);
+        }
     }
 }
