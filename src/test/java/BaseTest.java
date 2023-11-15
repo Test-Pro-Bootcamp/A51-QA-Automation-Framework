b9a6dc806e52f23514ecaf537292e73560cd97d6
aparajita135 <142899090+aparajita135@users.noreply.github.com>
2023-11-15 14:47:59 -0500

    Homework24
    Couldn't open any of the browser. What could be the reason?

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -5,7 +5,12 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
@@ -13,11 +18,13 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Parameters;
 
+import java.net.MalformedURLException;
+import java.net.URI;
 import java.time.Duration;
 
 public class BaseTest {
 
-    public WebDriver driver;
+    public static WebDriver driver;
     public String url = "https://qa.koel.app/";
     public WebDriverWait wait;
     public Actions actions;
@@ -25,23 +32,63 @@ public class BaseTest {
     @BeforeSuite
 
     static void setupClass() {
-        WebDriverManager.chromedriver().setup();
+            //WebDriverManager.chromedriver().setup();
     }
     @BeforeMethod
     @Parameters({"BaseURL"})
-      public void launchBrowser(String BaseURL) {
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--remote-allow-origins=*");
-        options.addArguments("--disable-notifications");
+      public void launchBrowser(String BaseURL) throws MalformedURLException{
+//        ChromeOptions options = new ChromeOptions();
+//        options.addArguments("--remote-allow-origins=*");
+//        options.addArguments("--disable-notifications");
 
-        driver = new ChromeDriver(options);
+        //driver = new ChromeDriver(options);
         //driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver = pickBrowser(System.getProperty("browser"));
         wait= new WebDriverWait(driver,Duration.ofSeconds(10));
 
         actions = new Actions(driver);
 
          url = BaseURL;
         navigateToPage(BaseURL);
+    }
+
+    public static WebDriver pickBrowser(String browser)throws MalformedURLException {
+        DesiredCapabilities caps = new DesiredCapabilities();
+        String gridURL= "http://192.168.86.147:4444";
+
+        switch(browser){
+            case "firefox":
+                WebDriverManager.firefoxdriver().setup();
+                return driver= new FirefoxDriver();
+
+            case "MicrosoftEdge":
+                WebDriverManager.edgedriver().setup();
+                EdgeOptions edgeOptions = new EdgeOptions();
+                edgeOptions.addArguments("--remote-allow-origins-*");
+                edgeOptions.addArguments("--disable-notifications");
+                return driver= new EdgeDriver(edgeOptions);
+
+            case "grid-edge":
+                caps.setCapability("browserName", "MicrosoftEdge");
+                        return driver =new RemoteWebDriver(URI.create(gridURL).toURL(),caps);
+
+            case "grid-firefox":
+                caps.setCapability("browserName","firefox");
+                      return driver = new RemoteWebDriver(URI.create(gridURL).toURL(),caps);
+
+            case "grid-chrome":
+                caps.setCapability("browserName","chrome");
+                return driver = new RemoteWebDriver(URI.create(gridURL).toURL(),caps);
+
+
+            default:
+                WebDriverManager.chromedriver().setup();
+                ChromeOptions options = new ChromeOptions();
+               options.addArguments("--remote-allow-origins=*");
+               options.addArguments("--disable-notifications");
+               return driver = new ChromeDriver(options);
+        }
+
     }
 
 
