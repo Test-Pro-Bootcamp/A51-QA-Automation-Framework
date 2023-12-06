43c38ca116efa404fda4e473b6bc3dad402364a2
Daria2906 <daria.chebotnyagina@testpro.io>
2023-12-06 12:51:41 -0600

    last change

--- src/test/java/CucumberRunner.java
+++ src/test/java/CucumberRunner.java
@@ -6,7 +6,7 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.DataProvider;
 
 @CucumberOptions(
-        features = {"src/test/resources/features/HomePage.feature"}
+        features = {"src/test/resources/features/HomePage.feature"},publish = true
 )
 public class CucumberRunner extends AbstractTestNGCucumberTests {
     private TestNGCucumberRunner testNGCucumberRunner;
