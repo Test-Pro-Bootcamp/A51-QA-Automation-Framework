2785a5242f580126a582fa2f9e87d064fcee782c
s-harristester <sade.harris@testpro.io>
2023-12-14 14:51:12 -0500

    A51 Homework21
    Selenium Action Class

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
@@ -15,11 +16,19 @@ import java.time.Duration;
 
 public class BaseTest {
 
-            public WebDriver driver = null;
+            public WebDriver driver;
             public String url = "https://qa.koel.app/";
             public WebDriverWait wait;
+            Actions actions;
 
-            @BeforeSuite
+
+
+
+    public BaseTest() {
+
+    }
+
+    @BeforeSuite
             static void setupClass() {
                 WebDriverManager.chromedriver().setup();
             }
@@ -32,6 +41,7 @@ public class BaseTest {
 
                 driver = new ChromeDriver(options);
                 wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+                actions = new Actions(driver);
                 driver.manage().window().maximize();
                 url=BaseURL;
                 navigateToPage();
